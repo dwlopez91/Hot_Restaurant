5d31d05e7ca2b3453a80db166d7634d8e66d5157
Christopher Biessener <cpbiessener@gmail.com>
2018-02-24 11:13:51 -0600

    basic server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
