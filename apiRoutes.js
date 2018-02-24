e3e076a8fa2a6423b5b3edb3fdf7ade56f499260
natejahnke <nathanjjahnke@gmail.com>
2018-02-24 11:45:28 -0600

    added arrays and route to apiroutes

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,2 +1,31 @@
-module.exports = function(app) {
-}
+module.exports = function (app) {}
+
+
+//GET TABLE
+
+var reservations = [{
+        routeName: "yoda",
+        name: "Yoda",
+        phone: "Jedi Master",
+        email: 900,
+        unique: 2000
+    },
+    {
+        routeName: "darthmaul",
+        name: "Darth Maul",
+        phone: "Sith Lord",
+        email: 200,
+        unique: 1200
+    },
+    {
+        routeName: "obiwankenobi",
+        name: "Obi Wan Kenobi",
+        phone: "Jedi Master",
+        email: 55,
+        unique: 1350
+    }
+];
+
+app.get("/reserve", function (req, res) {
+    res.json(reservations);
+});
