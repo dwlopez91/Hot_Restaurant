5a2e5ea9c2832e032e826398a4e09b5a24b415ea
natejahnke <nathanjjahnke@gmail.com>
2018-02-24 11:12:57 -0600

    deleted node modules, created express server and basic html routes

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var fs = require("fs");
 
 var app = express();
 var PORT = 3000;
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
 
 app.listen(PORT, function () {
