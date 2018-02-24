47b8eaea640a6d7bac48ac0980c0b199ef8c62be
Christopher Biessener <cpbiessener@gmail.com>
2018-02-24 12:52:54 -0600

    add home route

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,6 +1,10 @@
 var path = require("path");
 
 module.exports = function(app) {
+  // display the home page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./home.html"));
+  });
   // display the tables page
   app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "./tables.html"));
