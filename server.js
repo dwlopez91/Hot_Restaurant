2421dad737e06ed63a303eb78488adefc7a0265c
natejahnke <nathanjjahnke@gmail.com>
2018-02-24 11:43:25 -0600

    added path npm

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Tells node that we are creating an "express" server
 var app = express();
@@ -8,7 +9,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // set up the routes
@@ -16,6 +19,6 @@ require("./apiRoutes")(app);
 require("./htmlRoutes")(app);
 
 // Starts the server to begin listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
